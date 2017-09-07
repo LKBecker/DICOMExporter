558df7e1d49f92d386b7308377e4aa4f38e4418a
Lorenz Becker <lorenz.becker@postgrad.manchester.ac.uk>
2017-09-07 11:23:01 +0100

    Reinitialisation of repositiory with newest code

---
+++ DICOMExporter/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace DICOMExporter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.buttonBrowseDICOMDirectory = new System.Windows.Forms.Button();
+			this.buttonBrowseOutputDirectory = new System.Windows.Forms.Button();
+			this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+			this.textBoxOutputDirectory = new System.Windows.Forms.TextBox();
+			this.textBoxDICOMDirectory = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.buttonConvert = new System.Windows.Forms.Button();
+			this.checkBoxExtractMetadata = new System.Windows.Forms.CheckBox();
+			this.checkBoxCreateMetadataTable = new System.Windows.Forms.CheckBox();
+			this.checkBoxCopyAndRename = new System.Windows.Forms.CheckBox();
+			this.checkBoxCheckAndDeleteAfterCopy = new System.Windows.Forms.CheckBox();
+			this.panelProgressBar = new System.Windows.Forms.Panel();
+			this.labelProgressBar = new System.Windows.Forms.Label();
+			this.progressBarDICOMFiles = new System.Windows.Forms.ProgressBar();
+			this.panelProgressBar.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// buttonBrowseDICOMDirectory
+			// 
+			this.buttonBrowseDICOMDirectory.Location = new System.Drawing.Point(505, 10);
+			this.buttonBrowseDICOMDirectory.Name = "buttonBrowseDICOMDirectory";
+			this.buttonBrowseDICOMDirectory.Size = new System.Drawing.Size(75, 23);
+			this.buttonBrowseDICOMDirectory.TabIndex = 0;
+			this.buttonBrowseDICOMDirectory.Text = "Browse...";
+			this.buttonBrowseDICOMDirectory.UseVisualStyleBackColor = true;
+			this.buttonBrowseDICOMDirectory.Click += new System.EventHandler(this.buttonBrowseDirectory_Click);
+			// 
+			// buttonBrowseOutputDirectory
+			// 
+			this.buttonBrowseOutputDirectory.Location = new System.Drawing.Point(505, 41);
+			this.buttonBrowseOutputDirectory.Name = "buttonBrowseOutputDirectory";
+			this.buttonBrowseOutputDirectory.Size = new System.Drawing.Size(75, 23);
+			this.buttonBrowseOutputDirectory.TabIndex = 1;
+			this.buttonBrowseOutputDirectory.Text = "Browse...";
+			this.buttonBrowseOutputDirectory.UseVisualStyleBackColor = true;
+			this.buttonBrowseOutputDirectory.Click += new System.EventHandler(this.buttonBrowseDirectory_Click);
+			// 
+			// textBoxOutputDirectory
+			// 
+			this.textBoxOutputDirectory.Location = new System.Drawing.Point(99, 38);
+			this.textBoxOutputDirectory.Name = "textBoxOutputDirectory";
+			this.textBoxOutputDirectory.Size = new System.Drawing.Size(400, 20);
+			this.textBoxOutputDirectory.TabIndex = 2;
+			// 
+			// textBoxDICOMDirectory
+			// 
+			this.textBoxDICOMDirectory.Location = new System.Drawing.Point(99, 12);
+			this.textBoxDICOMDirectory.Name = "textBoxDICOMDirectory";
+			this.textBoxDICOMDirectory.Size = new System.Drawing.Size(400, 20);
+			this.textBoxDICOMDirectory.TabIndex = 3;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(3, 15);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(90, 13);
+			this.label1.TabIndex = 4;
+			this.label1.Text = "DICOM Directory:";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(6, 41);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(87, 13);
+			this.label2.TabIndex = 5;
+			this.label2.Text = "Output Directory:";
+			// 
+			// buttonConvert
+			// 
+			this.buttonConvert.Location = new System.Drawing.Point(505, 73);
+			this.buttonConvert.Name = "buttonConvert";
+			this.buttonConvert.Size = new System.Drawing.Size(75, 23);
+			this.buttonConvert.TabIndex = 6;
+			this.buttonConvert.Text = "Convert";
+			this.buttonConvert.UseVisualStyleBackColor = true;
+			this.buttonConvert.Click += new System.EventHandler(this.buttonConvert_Click);
+			// 
+			// checkBoxExtractMetadata
+			// 
+			this.checkBoxExtractMetadata.AutoSize = true;
+			this.checkBoxExtractMetadata.Location = new System.Drawing.Point(296, 87);
+			this.checkBoxExtractMetadata.Name = "checkBoxExtractMetadata";
+			this.checkBoxExtractMetadata.Size = new System.Drawing.Size(201, 17);
+			this.checkBoxExtractMetadata.TabIndex = 7;
+			this.checkBoxExtractMetadata.Text = "Export per-folder metadata files (slow)";
+			this.checkBoxExtractMetadata.UseVisualStyleBackColor = true;
+			// 
+			// checkBoxCreateMetadataTable
+			// 
+			this.checkBoxCreateMetadataTable.AutoSize = true;
+			this.checkBoxCreateMetadataTable.Location = new System.Drawing.Point(296, 64);
+			this.checkBoxCreateMetadataTable.Name = "checkBoxCreateMetadataTable";
+			this.checkBoxCreateMetadataTable.Size = new System.Drawing.Size(159, 17);
+			this.checkBoxCreateMetadataTable.TabIndex = 8;
+			this.checkBoxCreateMetadataTable.Text = "Export metadata table (slow)";
+			this.checkBoxCreateMetadataTable.UseVisualStyleBackColor = true;
+			// 
+			// checkBoxCopyAndRename
+			// 
+			this.checkBoxCopyAndRename.AutoSize = true;
+			this.checkBoxCopyAndRename.Location = new System.Drawing.Point(99, 64);
+			this.checkBoxCopyAndRename.Name = "checkBoxCopyAndRename";
+			this.checkBoxCopyAndRename.Size = new System.Drawing.Size(130, 17);
+			this.checkBoxCopyAndRename.TabIndex = 10;
+			this.checkBoxCopyAndRename.Text = "Copy and rename files";
+			this.checkBoxCopyAndRename.UseVisualStyleBackColor = true;
+			this.checkBoxCopyAndRename.CheckedChanged += new System.EventHandler(this.checkBoxCopyAndRename_CheckedChanged);
+			// 
+			// checkBoxCheckAndDeleteAfterCopy
+			// 
+			this.checkBoxCheckAndDeleteAfterCopy.AutoSize = true;
+			this.checkBoxCheckAndDeleteAfterCopy.Enabled = false;
+			this.checkBoxCheckAndDeleteAfterCopy.Location = new System.Drawing.Point(99, 87);
+			this.checkBoxCheckAndDeleteAfterCopy.Name = "checkBoxCheckAndDeleteAfterCopy";
+			this.checkBoxCheckAndDeleteAfterCopy.Size = new System.Drawing.Size(174, 17);
+			this.checkBoxCheckAndDeleteAfterCopy.TabIndex = 9;
+			this.checkBoxCheckAndDeleteAfterCopy.Text = "Compare then delete after copy";
+			this.checkBoxCheckAndDeleteAfterCopy.UseVisualStyleBackColor = true;
+			// 
+			// panelProgressBar
+			// 
+			this.panelProgressBar.Controls.Add(this.labelProgressBar);
+			this.panelProgressBar.Controls.Add(this.progressBarDICOMFiles);
+			this.panelProgressBar.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.panelProgressBar.Location = new System.Drawing.Point(0, 0);
+			this.panelProgressBar.Name = "panelProgressBar";
+			this.panelProgressBar.Size = new System.Drawing.Size(587, 105);
+			this.panelProgressBar.TabIndex = 11;
+			// 
+			// labelProgressBar
+			// 
+			this.labelProgressBar.AutoSize = true;
+			this.labelProgressBar.Location = new System.Drawing.Point(193, 71);
+			this.labelProgressBar.Name = "labelProgressBar";
+			this.labelProgressBar.Size = new System.Drawing.Size(137, 13);
+			this.labelProgressBar.TabIndex = 1;
+			this.labelProgressBar.Text = "Processing file 0 of 0 (0%)...";
+			this.labelProgressBar.Visible = false;
+			// 
+			// progressBarDICOMFiles
+			// 
+			this.progressBarDICOMFiles.Location = new System.Drawing.Point(140, 41);
+			this.progressBarDICOMFiles.Name = "progressBarDICOMFiles";
+			this.progressBarDICOMFiles.Size = new System.Drawing.Size(307, 23);
+			this.progressBarDICOMFiles.TabIndex = 0;
+			this.progressBarDICOMFiles.Visible = false;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(587, 105);
+			this.Controls.Add(this.checkBoxCopyAndRename);
+			this.Controls.Add(this.checkBoxCheckAndDeleteAfterCopy);
+			this.Controls.Add(this.checkBoxCreateMetadataTable);
+			this.Controls.Add(this.checkBoxExtractMetadata);
+			this.Controls.Add(this.buttonConvert);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.textBoxDICOMDirectory);
+			this.Controls.Add(this.textBoxOutputDirectory);
+			this.Controls.Add(this.buttonBrowseOutputDirectory);
+			this.Controls.Add(this.buttonBrowseDICOMDirectory);
+			this.Controls.Add(this.panelProgressBar);
+			this.HelpButton = true;
+			this.Name = "Form1";
+			this.Text = "DICOM Processor";
+			this.HelpButtonClicked += new System.ComponentModel.CancelEventHandler(this.Form1_HelpButtonClicked);
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.panelProgressBar.ResumeLayout(false);
+			this.panelProgressBar.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+        }
+
+        #endregion
+
+		private System.Windows.Forms.Button buttonBrowseDICOMDirectory;
+		private System.Windows.Forms.Button buttonBrowseOutputDirectory;
+		private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+		private System.Windows.Forms.TextBox textBoxOutputDirectory;
+		private System.Windows.Forms.TextBox textBoxDICOMDirectory;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Button buttonConvert;
+		private System.Windows.Forms.CheckBox checkBoxExtractMetadata;
+		private System.Windows.Forms.CheckBox checkBoxCreateMetadataTable;
+		private System.Windows.Forms.CheckBox checkBoxCopyAndRename;
+		private System.Windows.Forms.CheckBox checkBoxCheckAndDeleteAfterCopy;
+		private System.Windows.Forms.Panel panelProgressBar;
+		private System.Windows.Forms.ProgressBar progressBarDICOMFiles;
+		private System.Windows.Forms.Label labelProgressBar;
+    }
+}
+
---
+++ DICOMExporter/Form1.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace DICOMExporter {
+    public partial class Form1 : Form {
+		static int lostTagCount = 0;
+		Stack<DicomFile> DICOMFileStack = new Stack<DicomFile>();
+		char[] InvalidFileChars = Path.GetInvalidFileNameChars();
+		char[] InvalidPathChars = Path.GetInvalidPathChars();
+		const int BYTES_TO_READ = sizeof(Int64);
+
+        public Form1(){ 
+			InitializeComponent();
+		}
+
+        private void Form1_Load(object sender, EventArgs e){ 
+			this.Text = "DICOM Processor v" + Application.ProductVersion; 
+#if DEBUG
+			this.textBoxDICOMDirectory.Text = @"F:\Other People's Data\2017-03_SecondEchoPurge";
+			this.textBoxOutputDirectory.Text = @"\\nask.man.ac.uk\home$\Desktop\TestFolder";
+#endif
+		}
+
+		private void buttonBrowseDirectory_Click(object sender, EventArgs e) {
+			DialogResult result = folderBrowserDialog1.ShowDialog();
+			TextBox target = ((Button)sender).Name.Contains("DICOM") ? this.textBoxDICOMDirectory : this.textBoxOutputDirectory;
+			if (result == DialogResult.OK) { target.Text = folderBrowserDialog1.SelectedPath; }
+		}
+
+		private void buttonConvert_Click(object sender, EventArgs e) {
+			if (!checkBoxCreateMetadataTable.Checked && !checkBoxCopyAndRename.Checked && !checkBoxExtractMetadata.Checked) { MessageBox.Show("No checkboxes ticked - program has no task to perform."); return; }
+			if (textBoxDICOMDirectory.Text.Length == 0 || textBoxOutputDirectory.Text.Length == 0) { 
+				MessageBox.Show("Either Source or Destination directories not set. Cannot continue.", "Not all paths specified", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				return;
+			}
+			if (!Directory.Exists(textBoxDICOMDirectory.Text)) {
+				MessageBox.Show("Source directory not found: " + textBoxDICOMDirectory.Text, "Invalid Source Path", MessageBoxButtons.OK, MessageBoxIcon.Error); 
+				return;
+			}
+			if (!Directory.Exists(textBoxOutputDirectory.Text)) {
+				MessageBox.Show("Output directory not found: " + textBoxOutputDirectory.Text, "Invalid Output Path", MessageBoxButtons.OK, MessageBoxIcon.Error); 
+				return;
+			}
+
+			panelProgressBar.BringToFront();
+			panelProgressBar.Visible = true;
+			labelProgressBar.BringToFront();
+			labelProgressBar.Visible = true;
+			progressBarDICOMFiles.BringToFront();
+			progressBarDICOMFiles.Visible = true;
+
+			try { TraverseDirectoryTree(textBoxDICOMDirectory.Text); }
+
+			finally {
+				panelProgressBar.SendToBack();
+				panelProgressBar.Visible = false;
+				labelProgressBar.SendToBack();
+				labelProgressBar.Visible = false;
+				progressBarDICOMFiles.SendToBack();
+				progressBarDICOMFiles.Visible = false;
+			}
+		}
+
+		private void TraverseDirectoryTree(string root) {
+			StreamWriter MetadataTable = null;
+			int errorCount = 0, copyCount = 0, deleteCount = 0, extractCount = 0, tableCount = 0;
+			lostTagCount = 0;
+			Stack<string> dirs = new Stack<string>(100);
+			List<string> files = new List<string>();
+			if (checkBoxCreateMetadataTable.Checked) {
+				MetadataTable = new StreamWriter(Path.Combine(textBoxOutputDirectory.Text, DateTime.Now.ToString("yyyy-MM-dd_HH-mm") + "_DICOMFilesMetadata-Table.txt"), false);
+				MetadataTable.WriteLine("Patient ID\tPatient Name\tStudy Date\tStudy Time\tData Type\tFrames");
+				MetadataTable.Flush();
+			}
+
+			//Acquisition
+			if (!Directory.Exists(root)) { throw new ArgumentException("Specified root directory doesn't exist: " + root); }
+			dirs.Push(root);
+			while (dirs.Count > 0) {
+				string currentDir = dirs.Pop();
+	
+				string[] subDirs;
+				try { subDirs = Directory.GetDirectories(currentDir); }
+				catch (UnauthorizedAccessException) { continue; }
+				catch (DirectoryNotFoundException) { continue; }
+				foreach (string str in subDirs) { dirs.Push(str); }				// Push subdirectories on stack for traversal.
+
+				string[] DICOMFiles = null;
+				try { DICOMFiles = Directory.GetFiles(currentDir, "*.dcm"); }
+				catch (UnauthorizedAccessException) { continue; }
+				catch (DirectoryNotFoundException) { continue; }
+				foreach (string file in DICOMFiles) { files.Add(file); }
+			} // while dirs.Count > 0
+
+			//Processing
+
+			bool DoSimpleParse = !(checkBoxCreateMetadataTable.Checked || checkBoxExtractMetadata.Checked);
+
+			int maxFiles = files.Count;
+			for (int i = 0; i < maxFiles; i++) {
+				float progress = ((float)(i+1) / maxFiles);
+				labelProgressBar.Text = string.Format("Processing file {0} of {1} ({2:p0})", i+1, maxFiles, progress);
+				progressBarDICOMFiles.Value = (int)Math.Floor(progress * 100);
+				Application.DoEvents(); //keeps GUI alive by returning control to OS thread
+
+				string DICOMFile = files[i];
+				string NewFolderName, NewFileName, StudyTime, PatientName, StudyDate, PatientID;
+				int nFrames;
+				DICOM.File dcf = null;
+				try { //Take and open file, extract metadata
+					dcf = new DICOM.File(DICOMFile, DoSimpleParse);
+					PatientName = TagValueOrDefault(0x00100010, dcf.Tags);
+					PatientID = TagValueOrDefault(0x00100020, dcf.Tags);
+					StudyDate = TagValueOrDefault(0x00080023, dcf.Tags);
+					StudyTime = TagValueOrDefault(0x00080033, dcf.Tags);
+					if (!DoSimpleParse) { nFrames = int.Parse(TagValueOrDefault(0x00280008, dcf.Tags, "1")); }
+					else { nFrames = ((new FileInfo(DICOMFile).Length / 1024) > 811) ? 125 : 1; } //single-image files are always 811KB big, so 811 * 1024 bytes
+					NewFolderName = string.Format("{0}_{1}_{2}", StudyDate, PatientID, PatientName);
+				}// try
+				catch (FileNotFoundException) { errorCount++; continue; }
+				
+				if (checkBoxCreateMetadataTable.Checked) {
+					//"Patient ID\tPatient Name\tStudy Date\tStudy Time\tData Type\tFrames"
+					MetadataTable.WriteLine(string.Format("{0}\t{1}\t{2}\t{3}\t{4}\t{5}", PatientID, PatientName, StudyDate, StudyTime, (nFrames > 1 ? "Movie" : "Image"), nFrames));
+					tableCount++;
+					MetadataTable.Flush();
+				}
+
+				foreach (char c in InvalidPathChars) { NewFolderName = NewFolderName.Replace(c.ToString(), ""); } //sanitize path input
+				foreach (char c in new char[] { '/', '\\', '?' }) { NewFolderName = NewFolderName.Replace(c.ToString(), "-"); } //these aren't in InvalidPathChars since they're allowed in (full) paths
+				DirectoryInfo OutputFolder = new DirectoryInfo(Path.Combine(textBoxOutputDirectory.Text, NewFolderName));
+				if (checkBoxCopyAndRename.Checked || checkBoxExtractMetadata.Checked) {
+					//prepare receiving folder
+					if (!Directory.Exists(OutputFolder.FullName)) {
+						try { Directory.CreateDirectory(OutputFolder.FullName); }
+						catch (UnauthorizedAccessException) {
+							MessageBox.Show("Cannot gain access to " + textBoxOutputDirectory.Text + ". Please select another directory or run as administrator.");
+							return;
+						}
+					}//if FolderName doesn't exist
+				}
+
+				if (checkBoxCopyAndRename.Checked) {
+					//Ensure numbering is consistent and continuous
+					int nPictures = 1, nVideos = 1;
+					FileInfo[] ExistingFiles = OutputFolder.GetFiles("*.dcm");
+					nPictures += ExistingFiles.Where(n => n.Name.Contains("Image")).Count();
+					nVideos += ExistingFiles.Where(n => n.Name.Contains("Video")).Count();
+					NewFileName = string.Format("{0}_{1}_{2}.dcm", NewFolderName, StudyTime, (nFrames > 1 ? ("Video" + nVideos) : ("Image" + nPictures)));
+					foreach (char c in InvalidFileChars) { NewFileName.Replace(c.ToString(), ""); }
+
+					//Copy File, Test File, if there's a request, Delete File
+					string NewFilePath = Path.Combine(OutputFolder.FullName, NewFileName);
+					if(File.Exists(NewFilePath)) { //Need to change name not to overwrite; safety first
+						int fileCount = OutputFolder.GetFiles(Path.GetFileNameWithoutExtension(NewFileName) + "*").Length + 1;
+						NewFileName = string.Format("{0}({1}){2}", Path.GetFileNameWithoutExtension(NewFileName), fileCount, Path.GetExtension(NewFileName));
+						NewFilePath = Path.Combine(OutputFolder.FullName, NewFileName);
+					} //File.Exists(NewFilePath)
+
+					try {
+						File.Copy(DICOMFile, NewFilePath);
+						copyCount++;
+					}
+					catch (UnauthorizedAccessException) { MessageBox.Show("Access denied for export directory. Please select another directory or restart as administrator."); return; }
+					catch (IOException ioe) { MessageBox.Show("Unexpected IO Exception: " + ioe.Message); }
+
+					Application.DoEvents();
+
+					if (checkBoxCheckAndDeleteAfterCopy.Checked) {
+						if (TestFileEquality(new FileInfo(DICOMFile), new FileInfo(NewFilePath))) {
+							File.Delete(DICOMFile);
+							deleteCount++;
+						}
+					} //checkBoxCheckAndDeleteAfterCopy.Checked
+
+				}//if checkboxCopyAndRename.Checked
+
+				if (checkBoxExtractMetadata.Checked) {
+					using (StreamWriter metadataFile = new StreamWriter(Path.Combine(OutputFolder.FullName, "metadata.txt"), true)) {
+						metadataFile.WriteLine("Metadata extracted from " + DICOMFile +"\nTag\tValue");
+						foreach (DICOM.Tag tag in dcf.Tags) { metadataFile.WriteLine(tag.Name + "\t" + tag.Value); } //TODO something fucky here.
+						metadataFile.WriteLine("\n");
+						extractCount++;
+					}
+				}//if checkboxExtractMetadata.Checked
+			}//foreach DICOMFile
+
+			if (checkBoxCreateMetadataTable.Checked) { MetadataTable.Close(); } //cleanup
+
+			//Generating success message to user
+			string Report = "Operation(s) completed successfully\n";
+			string LostTagReport = lostTagCount > 0 ? lostTagCount + " values could not found and defaulted to 'undefined'.\n" : "";
+			if (checkBoxCopyAndRename.Checked) { Report += copyCount + " files copied and renamed. " + LostTagReport; }
+			if (checkBoxCheckAndDeleteAfterCopy.Checked) { Report += deleteCount + " files deleted.\n"; }
+			if (checkBoxExtractMetadata.Checked) { Report += "Metadata extracted for " + extractCount + " files. " + LostTagReport;  }
+			if (checkBoxCreateMetadataTable.Checked) { Report += tableCount + " files processed for metadata table. " + LostTagReport; }
+			progressBarDICOMFiles.Value = 100;
+			Application.DoEvents();
+			this.BringToFront();
+			MessageBox.Show(Report, "DICOM processing complete.");
+		} // TraverseDirectoryTree
+
+		private void Form1_HelpButtonClicked(object sender, CancelEventArgs e) {
+			MessageBox.Show("DICOM Processor Help", "DICOM Processor is created and optimised for use with the specific model of GE Echocardiograph stationed in the University of Manchester BSF." +
+				"There is no warranty, express or implied. Use at your own risk. Back up your data regularly.");
+		}
+
+		private void checkBoxCopyAndRename_CheckedChanged(object sender, EventArgs e) {
+			bool state = ((CheckBox)sender).Checked;
+			if (!state) { checkBoxCheckAndDeleteAfterCopy.Checked = false; }
+			checkBoxCheckAndDeleteAfterCopy.Enabled = state;
+		}
+		private string TagValueOrDefault(int TagKey, List<DICOM.Tag> SearchSpace, string defaultValue = "Undefined") {
+			try {
+				DICOM.Tag tag = SearchSpace.Find(n => n.GroupElement == TagKey);
+				return tag.Value;
+			}
+			catch {
+				if (TagKey != 0x00280008) { lostTagCount++; } //we expect not to see Number of Frames in Image Files (single frame, but not listed)
+				return defaultValue; 
+			}
+		}
+
+		private bool TestFileEquality(FileInfo file1, FileInfo file2) {
+			if (file1.Length != file2.Length) { return false; }
+			int iterations = (int)Math.Ceiling((double)file1.Length / BYTES_TO_READ);
+			using (FileStream fs1 = file1.OpenRead(), fs2 = file2.OpenRead()) {
+				byte[] one = new byte[BYTES_TO_READ];
+				byte[] two = new byte[BYTES_TO_READ];
+				for (int i = 0; i < iterations; i++) {
+					fs1.Read(one, 0, BYTES_TO_READ);
+					fs2.Read(two, 0, BYTES_TO_READ);
+					if (BitConverter.ToInt64(one, 0) != BitConverter.ToInt64(two, 0)) { return false; }//tripped at the first mismatch
+				}// for iterations
+			}//using FileStream fs1 and FileStream fs2
+			return true;
+		} //bool TestFileEquality
+	} //class Form 1
+} // namespace DICOMExporter
